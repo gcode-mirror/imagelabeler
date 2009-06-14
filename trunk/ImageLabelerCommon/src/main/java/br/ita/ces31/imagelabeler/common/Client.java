cc44de7331dd40fb0c76547f006c91480160fc1e
dalvac <dalvac@cf63259e-3d2d-11de-8f11-b9fadefa0f10>
2009-06-14 09:23:58 +0000

    renomeando pacotes

--- trunk/ImageLabelerCommon/src/main/java/br/ita/ces31/imagelabeler/common/Client.java
+++ trunk/ImageLabelerCommon/src/main/java/br/ita/ces31/imagelabeler/common/Client.java
@@ -1,7 +1,7 @@
 /*
  * Image Labeler - Projeto de CES-31
  */
-package br.ita.ces31.ImageLabelerCommon;
+package br.ita.ces31.imagelabeler.common;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
