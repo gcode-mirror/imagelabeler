dd33cab9ad59bbd7fe8a8a540395f7cd3fb11bf5
dalvac <dalvac@cf63259e-3d2d-11de-8f11-b9fadefa0f10>
2009-06-14 09:23:58 +0000

    renomeando pacotes

--- ImageLabelerCommon/src/main/java/br/ita/ces31/imagelabeler/common/GameSummary.java
+++ ImageLabelerCommon/src/main/java/br/ita/ces31/imagelabeler/common/GameSummary.java
@@ -1,7 +1,7 @@
 /*
  * Image Labeler - Projeto de CES-31
  */
-package br.ita.ces31.ImageLabelerCommon;
+package br.ita.ces31.imagelabeler.common;
 
 import java.util.ArrayList;
 import java.util.List;
