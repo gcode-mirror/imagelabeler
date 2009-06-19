953eda693eaed9c5ce00ab881d28eaa12dd014db
heldersuzuki <heldersuzuki@cf63259e-3d2d-11de-8f11-b9fadefa0f10>
2009-06-19 04:25:32 +0000

    LogClient

---
+++ trunk/ImageLabelerClient/src/main/java/br/ita/ces31/imagelabeler/client/LogClient.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2009 Helder Suzuki <heldersuzuki@gmail.com>.
+ */
+
+package br.ita.ces31.imagelabeler.client;
+
+import br.ita.ces31.imagelabeler.client.communicator.CommunicatorObserver;
+import br.ita.ces31.imagelabeler.common.GameSummary;
+
+/**
+ *
+ * @author Helder Suzuki <heldersuzuki@gmail.com>
+ */
+class LogClient implements CommunicatorObserver{
+
+    public LogClient() {
+    }
+
+    public void endGameByPenico() {
+        System.out.println("----------------------------------");
+        System.out.println("endGameByPenico()");
+       System.out.println("----------------------------------");
+    }
+
+    public void notifyLabelMatch(String match, int score) {
+        System.out.println("----------------------------------");
+        System.out.println("notifyLabelMatch");
+        System.out.println("----------------------------------");
+    }
+
+    public void startGame(String image, String partner) {
+        System.out.println("----------------------------------");
+        System.out.println("startGame");
+        System.out.println("----------------------------------");
+    }
+
+    public void endGame(GameSummary summary) {
+        System.out.println("endGame");
+    }
+
+}
